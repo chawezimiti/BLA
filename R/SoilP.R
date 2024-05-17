e33f247824f6e2d38674b2547ab3c4d025e2d4a3
chawezimiti <chawezi.miti@nottingham.ac.uk>
2024-05-17 21:29:30 +0100

    Two new datasets added which are subsets of the AgSpace data

---
+++ R/SoilP.R
@@ -0,0 +1,15 @@
+#' Soil Phosphorus data
+#'
+#' This data set is a subset of a data set that was assembled by AgSpace Agriculture Ltd
+#' in a soil survey conducted on farms in various management units across England.
+#'
+#' @format A data.frame with 6020 rows and 2 columns:
+#' \describe{
+#' \item{yield}{ Wheat yield (ton/ha) measured}.
+#' \item{P}{ Phosphorus (mg/l)}.
+#' }
+#’
+#'
+#' @examples
+#' data(SoilP)
+"SoilP"
---
+++ R/SoilpH.R
@@ -0,0 +1,15 @@
+#' Soil pH data
+#'
+#' This data set is a subset of a data set that was assembled by AgSpace Agriculture Ltd
+#' in a soil survey conducted on farms in various management units across England.
+#'
+#' @format A data.frame with 6047 rows and 2 columns:
+#' \describe{
+#' \item{yield}{ Wheat yield (ton/ha) measured}.
+#' \item{pH}{ pH measurement}.
+#' }
+#’
+#'
+#' @examples
+#' data(SoilpH)
+"SoilpH"
